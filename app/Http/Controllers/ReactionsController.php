59ed440f26e976c09d6d5d21b17da85db090dd2c
DraganJankovic997 <gagibratee@gmail.com>
2019-08-20 16:06:25 +0200

    Search implemented

--- app/Http/Controllers/ReactionsController.php
+++ app/Http/Controllers/ReactionsController.php
@@ -6,19 +6,36 @@ use App\Reaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Emote;
+use Illuminate\Support\Facades\Auth;
+
+
 class ReactionsController extends Controller
 {
     public function react(Request $request) {
-        
-        return Reaction::create($request->all());
+        $data = array_merge(['user_id' => Auth::id()], $request->all());
+        $old = Reaction::where('user_id', $data['user_id'])->where('movie_id', $data['movie_id'])->first();
+        if( $old === null) {
+            return Reaction::create($data);
+        } else {
+            return 'You already rated this movie';
+        }
     }
 
     public function reactions($id){
-        return Reaction::where('movie_id', $id)
-            ->join('emotes as e', 'e.id', '=', 'reactions.emote_id')
-            ->select('movie_id', 'e.name', DB::raw('count(*) as reaction_count'))
-            ->groupBy('e.name')
-            ->groupBy('movie_id')
-            ->get();    
+        $emotes = Emote::all();
+        $emotesNumber = $emotes->map(function ($emote) use ($id) {
+            return Reaction::where([
+                'movie_id' => $id,
+                'emote_id' => $emote->id,
+            ])
+            ->select('movie_id', 'e.name', 'e.id as emote_id', DB::raw('count(*) as reaction_count'))
+            ->count();
+
+        });
+        $reactions = [
+            'movie_id' => $id,
+            'emotes' => $emotesNumber
+        ];
+        return $reactions;
     }
 }
